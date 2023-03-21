1ec8d0a824ef18b8742d45ea6891ae347a92f5ff
Vitalii Sliusar <slusarvitaliy@gmail.com>
2023-03-21 21:11:45 +0200

    added 404

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Page;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PageController extends Controller
 {
@@ -12,8 +14,13 @@ class PageController extends Controller
         $page = Page::query()
             ->where('slug', $slug)
             ->where('is_enabled', true)
+            ->where('created_at', '<=',Carbon::now())
             ->with('media')
             ->first();
+
+        if(!$page) {
+            throw new NotFoundHttpException();
+        }
 
         if($page) {
             $page->thumb = $page->getMedia('thumbs')->first();
